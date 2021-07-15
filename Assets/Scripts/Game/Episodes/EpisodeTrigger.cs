afea41fbaef61e957751f99586a61807e433ab6f
Ivan Siluianov <ivan.siluianov@gmail.com>
2021-07-15 22:07:10 +0200

    Add a transition between episodes.

--- Assets/Scripts/Game/Episodes/EpisodeTrigger.cs
+++ Assets/Scripts/Game/Episodes/EpisodeTrigger.cs
@@ -1,18 +1,45 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Game.Episodes
 {
 	public class EpisodeTrigger : MonoBehaviour
 	{
 		public event Action PlayerEnteredTheTrigger;
+		private Animation anim;
+		private bool _haveCollision;
+		private bool _canChangeScene;
+		private float _timeLeft = 1.5f;
 
 		[SerializeField] private int _episodeNumber;
-		
-		private void OnTriggerEnter2D(Collider2D other)
+		[SerializeField] private GameObject _otherGameObject;
+
+        private void Start()
+        {
+            anim = _otherGameObject.GetComponent<Animation>();
+        }
+
+        private void Update()
+        {
+            if (_haveCollision)
+            {
+				_timeLeft -= Time.deltaTime;
+				if (_timeLeft < 0)
+                {
+					SceneManager.LoadScene(_episodeNumber);
+					_haveCollision = false;
+					_timeLeft = 1.5f;
+				}
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
 		{
 			if (other.gameObject.CompareTag("PPlayer"))
 			{
+				anim.Play();
+				_haveCollision = true;
 				PlayerEnteredTheTrigger?.Invoke();
 			}
 		}
